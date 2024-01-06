82b2e19515ea806957eeaeb277a705477be762d4
Muhammad Aliza Mudrikah <alzzamuhammad77@gmail.com>
2024-01-06 16:17:48 +0700

    solving problem on new campground

--- controllers/campground.js
+++ controllers/campground.js
@@ -5,7 +5,7 @@ const mapBoxToken = process.env.MAPBOX_TOKEN;
 const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 
 module.exports.index = async (req, res) => {
-  const campgrounds = await Campground.find({});
+  const campgrounds = await Campground.find({}).populate('popupText');
   res.render('campgrounds/index', { campgrounds });
 };
 
@@ -20,13 +20,11 @@ module.exports.createCampground = async (req, res, next) => {
       limit: 1,
     })
     .send();
-  res.send('Ok!!');
   const campground = new Campground(req.body.campground);
   campground.geometry = geoData.body.features[0].geometry;
   campground.images = req.files.map((f) => ({ url: f.path, filename: f.filename }));
   campground.author = req.user._id;
   await campground.save();
-  console.log(campground);
   req.flash('success', 'Successfully made a new campground!');
   res.redirect(`/campgrounds/${campground._id}`);
 };
