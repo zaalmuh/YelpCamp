4b3894b7c6a44dd4daea2ef721f386baa4c83b4e
Muhammad Aliza Mudrikah <alzzamuhammad77@gmail.com>
2024-01-06 20:15:07 +0700

    Fixing logout error

--- controllers/users.js
+++ controllers/users.js
@@ -32,7 +32,11 @@ module.exports.login = (req, res) => {
 };
 
 module.exports.logout = (req, res) => {
-  req.logout();
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+  });
   req.flash('success', 'Goodbye!');
   res.redirect('/campgrounds');
 };
